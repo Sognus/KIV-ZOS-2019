08094e80be5b0e39a13990cdfa36f7f81348f2c4
Jakub Vítek <sognus@sognus.cz>
2020-01-17 00:31:48 +0100

    Oprava kompilace pro Win

--- commands.c
+++ commands.c
@@ -10,6 +10,68 @@
 #include "file.h"
 #include "symlink.h"
 
+
+// Just because Windows is stupid
+#ifdef  _WIN32
+    #include <errno.h>
+
+    /**
+     * Reimplementace pro NON-POSIX systémy (Windows)
+     *
+     * @param lineptr
+     * @param n
+     * @param stream
+     * @return
+     */
+    ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+        size_t pos;
+        int c;
+
+        if (lineptr == NULL || stream == NULL || n == NULL) {
+            errno = EINVAL;
+            return -1;
+        }
+
+        c = getc(stream);
+        if (c == EOF) {
+            return -1;
+        }
+
+        if (*lineptr == NULL) {
+            *lineptr = malloc(128);
+            if (*lineptr == NULL) {
+                return -1;
+            }
+            *n = 128;
+        }
+
+        pos = 0;
+        while(c != EOF) {
+            if (pos + 1 >= *n) {
+                size_t new_size = *n + (*n >> 2);
+                if (new_size < 128) {
+                    new_size = 128;
+                }
+                char *new_ptr = realloc(*lineptr, new_size);
+                if (new_ptr == NULL) {
+                    return -1;
+                }
+                *n = new_size;
+                *lineptr = new_ptr;
+            }
+
+            ((unsigned char *)(*lineptr))[pos ++] = c;
+            if (c == '\n') {
+                break;
+            }
+            c = getc(stream);
+        }
+
+        (*lineptr)[pos] = '\0';
+        return pos;
+    }
+#endif
+
 /**
  * Příkaz PWD
  * @param sh kontext virtuálního terminálu
